ca286d27c7b9d84bd1d24355c28e3f2241345320
VietDucFCB <116992108+VietDucFCB@users.noreply.github.com>
2024-09-13 21:00:58 +0700

    Add files via upload

---
+++ Second_Large_In_Dynamic_Array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void SapXep(vector<int> A; int n){
+	for(int i=1; i<n-1; i++){
+		for(int j=i+1; ; i<n; j++){
+			if(a[i]>a[j])
+				swap(a[i], a[j])
+		}
+	}
+}
+int Nto(int x){
+	for(int i=2; i<=sqrt(x); i++){
+		if(x%i!=0)
+			return 1;
+		else return 0;
+	}
+}
+int main(){
+  vector<int> A;
+  cout << "Nhap so phan tu trong mang: ";
+  int SoPtu;
+  cin >> Ptu;
+  for (int i = 0; i < SoPtu; i++) {
+    int Ptu;
+    std::cout << "Nhap Phan tu " << i << ": ";
+    std::cin >> Ptu;
+    A.push_back(Ptu);
+  }
+
+  return 0;
+}
